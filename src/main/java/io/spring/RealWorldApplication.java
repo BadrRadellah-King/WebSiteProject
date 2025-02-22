562c53aa2bec5ca8121f9a4d1c4e14233300a3a6
Badr <badrradellah@gmail.com>
2025-02-22 21:55:32 +0100

    Ready

--- src/main/java/io/spring/RealWorldApplication.java
+++ src/main/java/io/spring/RealWorldApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealWorldApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(RealWorldApplication.class, args);
+
   }
 }
